8e104bc0959d5b4fb3c295174199794c307874e3
JackLovel <2101721230@qq.com>
2019-07-12 19:00:54 +0800

    copy from Rekols

---
+++ LearnRekolsDemo/ExpressInquiry/main.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    MainWindow w;
+    w.setWindowTitle("快递查询");
+    w.show();
+
+    return a.exec();
+}
